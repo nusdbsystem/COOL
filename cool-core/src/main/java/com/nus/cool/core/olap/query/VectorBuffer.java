ed1137c2eba13123cf9bf0920f48588d7b67d05b
NLGithubWP <xingnaili14@gmail.com>
2022-03-04 11:19:16 +0800

    Add vector buffer

---
+++ cool-core/src/main/java/com/nus/cool/core/olap/query/VectorBuffer.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.nus.cool.core.olap.query;
+
+/**
+ * @author david
+ *
+ */
+public class VectorBuffer {
+	
+	public boolean isSelectorInUse;
+	
+	public int[] selector;
+	
+	public int size;
+	
+	public VectorColumn[] vecs;
+
+}
