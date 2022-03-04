31bde667b5a2fe3f02f3cab4c01c2ea4c44c0785
NLGithubWP <xingnaili14@gmail.com>
2022-03-04 14:20:07 +0800

    Add Shorts.java

---
+++ cool-core/src/main/java/com/nus/cool/core/lang/Shorts.java
@@ -0,0 +1,37 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.nus.cool.core.lang;
+
+import java.nio.ByteOrder;
+
+public class Shorts {
+
+	/**
+	 * Convert the input value into OS's native byte order
+	 * 
+	 * @param v
+	 *          the integer
+	 * @return the @param v in native order
+	 */
+	public static short toNativeByteOrder(short v) {
+		boolean bLittle = (ByteOrder.nativeOrder() == ByteOrder.LITTLE_ENDIAN);
+		return (bLittle ? Short.reverseBytes(v) : v);
+	}
+	
+}
